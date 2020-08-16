9afeabec0f97c66c80be0c6d3f212c8d5d2dfa04
John McCormack <silentsammy777@hotmail.com>
2020-08-16 01:35:57 +0100

    Added Patrol

---
+++ Assets/Scripts/AI/SteveAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteveAI : MonoBehaviour
+{
+    public Transform goal;
+    public Transform[] patrolPoints;
+    public float targetDistance;
+    public float moveSpeed;
+
+    private int currentWaypoint = 0;
+
+    private void Move()
+    {
+        Vector3 targetDir = goal.position - transform.position;
+        if(Vector3.Distance(transform.position, goal.position) > targetDistance)
+            transform.Translate(targetDir.normalized * moveSpeed);
+    }
+
+    void Patrol()
+    {
+        goal = patrolPoints[currentWaypoint];
+        Vector3 targetDir = goal.position - transform.position;
+        if (Vector3.Distance(transform.position, goal.position) > targetDistance)
+            transform.Translate(targetDir.normalized * moveSpeed);
+        else
+            if(currentWaypoint <= patrolPoints.Length)
+                currentWaypoint++;
+            else
+                currentWaypoint = 0;
+    }
+
+    private void LateUpdate()
+    {
+        Patrol();
+    }
+}
