d5f7e001dbbd5714df721dcc795aa03996d65c6c
John McCormack <silentsammy777@hotmail.com>
2020-08-18 19:21:35 +0100

    ai stuff

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+
+    private void Start()
+    {
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[7];
+    }
+
+    public void GoToHeli()
+    {
+
+    }
+}
